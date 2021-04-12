2a13bb3f5a044868a97686227f830dcb6fb51221
Jack Morgan <jack@ehelion.com>
2021-04-12 16:09:31 -0400

    Move main entry point code into __main__ module
    Delete cli module file
    Remove top level module import of "create_app"

--- kijiji_manager/__main__.py
+++ kijiji_manager/__main__.py
@@ -1,4 +1,22 @@
-from . import cli
+import argparse
+
+from .app import create_app
+
+
+def main():
+    """Entry point when module is run from an interactive prompt
+    e.g. 'python -m kijiji_manager' or from console script"""
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c', '--config', help='path to config file')
+    parser.add_argument('-b', '--bind', help='interface to bind to (default: localhost)')
+    parser.add_argument('-p', '--port', type=int, help='port to bind to (default: 5000)')
+    parser.add_argument('-d', '--debug', action='store_true', help='enable debugging')
+    args = parser.parse_args()
+
+    app = create_app(args.config)
+    app.run(host=args.bind, port=args.port, debug=args.debug)
+
 
 if __name__ == '__main__':
-    cli.run()
+    main()
