50f570d9e8ba789de0caf7f3454f4d81995f87c3
jackm <jack@ehelion.com>
2021-11-28 12:44:29 -0500

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', encoding='utf-8') as f:
 
 setup(
     name='kijiji-manager',
-    version='0.1.4',
+    version='0.1.5',
     author='jackm',
     author_email='jackm@ehelion.com',
     description='App for viewing, posting, reposting, and deleting your Kijiji ads',
