c9a299cd8a8bd875327dc64bb903532ad0c0f40b
jackm <jack@ehelion.com>
2022-05-27 16:27:46 -0400

    Add back in Gunicorn config variables set by Docker image
    Using a custom Gunicorn config overwrites the default one provided by
    the tiangolo/meinheld-gunicorn Docker image.

--- gunicorn_conf.py
+++ gunicorn_conf.py
@@ -1,2 +1,16 @@
+import multiprocessing
+import os
+
+workers_per_core_str = os.getenv("WORKERS_PER_CORE", "2")
+host = os.getenv("HOST", "0.0.0.0")
+port = os.getenv("PORT", "80")
+
+cores = multiprocessing.cpu_count()
+workers_per_core = float(workers_per_core_str)
+web_concurrency = int(workers_per_core * cores)
+
 # Gunicorn config variables
+workers = web_concurrency
+bind = f"{host}:{port}"
+keepalive = 120
 timeout = 120
